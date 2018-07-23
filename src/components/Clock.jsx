8d164f61400af0ebb59cad8ca0c5716aebdbd260
wangjianfei <444128852@qq.com>
2018-07-23 13:16:38 +0800

    生命週期

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,29 +1,45 @@
 import React from "react"
 
 class Clock extends React.Component {
-    // 1
     constructor(props) {
         super(props)
         console.log(props, "Clock")
         // 使用this之前必须先：super()
         this.state = { date: new Date() }
     }
-    // 2 渲染到DOM之后执行（挂载）
+    // 1、将要挂在到DOM
+    componentWillMount() {
+        // Ajax 
+        console.log("componentWillMount")
+    }
+    // 2 渲染到DOM之后执行（挂载DOM之后）
     componentDidMount() {
         this.Timer = setInterval(
             () => this.autoChange(),
             1000
         )
     }
-    // 3、使用setState 周期性性的更新组件本地状态
+    // 3、组件将要更新
+    componentWillUpdate() {
+        // console.log("componentWillUpdate")
+    }
+    // 4、组件更新完毕
+    componentDidUpdate() {
+        // console.log("componentDidUpdate")
+    }
+    // 5、props将要更新
+    componentWillReceiveProps(nextProps) {
+        console.log("componentWillReceiveProps")
+    }
+    // 6、卸载
+    componentWillUnmount() {
+        if (this.Timer) clearInterval(this.Timer)
+    }
+    // 7、使用setState 周期性性的更新组件本地状态
     autoChange() {
         this.setState({
             date: new Date()
         })
-    }
-    // 4、卸载
-    componentWillUnmount() {
-        clearInterval(this.Timer)
     }
     render() {
         return (
--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -6,6 +6,32 @@ class CommentsList extends React.Component {
         console.log(props, "CommentsList")
     }
 
+    // 1、将要挂在到DOM
+    componentWillMount() {
+        // Ajax 
+        console.log("componentWillMount", "CommentsList")
+    }
+    // 2 渲染到DOM之后执行（挂载DOM之后）
+    componentDidMount() {
+        console.log("componentDidMount", "CommentsList")
+    }
+    // 3、组件将要更新
+    componentWillUpdate() {
+        console.log("componentWillUpdate", "CommentsList")
+    }
+    // 4、组件更新完毕
+    componentDidUpdate() {
+        console.log("componentDidUpdate", "CommentsList")
+    }
+    // 5、props将要更新
+    componentWillReceiveProps(nextProps) {
+        console.log(nextProps, "componentWillReceiveProps")
+    }
+    // 6、卸载
+    componentWillUnmount() {
+        console.log("componentWillUnmount", "CommentsList")
+    }
+
     render() {
         let list = (
             <ul>
